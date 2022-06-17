de976baa545faf4c24edb63c1b6f3f13ffef8c00
David Duarte <davidvermilion51gmail.com>
2022-06-16 20:09:10 -0500

    pokeApi y redux-toolkit

---
+++ src/store/slices/pokemon/thunks.js
@@ -0,0 +1,16 @@
+import { setPokemons, startLodingPokemons } from "./pokemonSlice"
+
+
+
+export const getPokemons = (page = 0) => {
+    return async( dispatch, getState ) => {
+        dispatch( startLodingPokemons() )
+
+        //TODO: Realizar peticion http
+        const resp = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${ page * 10 }`)
+        const data = await resp.json();
+        
+
+        dispatch( setPokemons({ pokemons: data.results, page: page + 1}) )
+    }
+} 
